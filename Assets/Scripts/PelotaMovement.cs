63dbdb812215fc7a80c967da0d5ce71232e2aa40
LucaFedericoMarty <101400526+LucaFedericoMarty@users.noreply.github.com>
2022-05-31 14:39:41 -0300

    Initial commit

---
+++ Assets/Scripts/PelotaMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PelotaMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.eulerAngles += new Vector3(0, 2.5f, 0);
+
+            if (Input.GetKeyUp(KeyCode.Mouse1))
+            {
+                transform.Translate(0.5f, 0, 0);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.eulerAngles -= new Vector3(0, 2.5f, 0);
+
+            if (Input.GetKeyUp(KeyCode.Mouse1))
+            {
+                transform.Translate(0.5f, 0, 0);
+            }
+        }
+    }
+}

7d344d2293ae4b8686314265aa0fe8ccecb3ef5e
LucaFedericoMarty <101400526+LucaFedericoMarty@users.noreply.github.com>
2022-06-01 15:44:35 -0300

    Movimiento de Pelota Act

--- Assets/Scripts/PelotaMovement.cs
+++ Assets/Scripts/PelotaMovement.cs
@@ -4,37 +4,62 @@ using UnityEngine;
 
 public class PelotaMovement : MonoBehaviour
 {
+    //Variable Bool
     bool ganaste;
+    bool isPressing;
+
+    // Variables Int
     int cantidadDeTiros;
+
+    // Variable Float
+    float timeElapsed;
+    float horizonatlSpeed = 2.0f;
+    float rotation;
+    float translation;
 
     // Start is called before the first frame update
     void Start()
     {
+        isPressing = false;
         ganaste = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    transform.eulerAngles += new Vector3(0, 2.5f, 0);
+        //}
+
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    transform.eulerAngles -= new Vector3(0, 2.5f, 0);
+
+        //}
+
+        //if (Input.GetKey(KeyCode.Space))
+        //{
+        //    transform.Translate(0.1f, 0, 0);
+        //}
+
+        if (isPressing == false)
         {
-            transform.eulerAngles += new Vector3(0, 2.5f, 0);
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                timeElapsed += Time.deltaTime;
+                rotation = horizonatlSpeed * Input.GetAxis("Mouse Y");
+                translation = Input.GetAxis("Mouse X") * timeElapsed;
+                isPressing = true;
+            }
         }
 
-        if (Input.GetKey(KeyCode.A))
+        if (isPressing)
         {
-            transform.eulerAngles -= new Vector3(0, 2.5f, 0);
-
-        }
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            transform.Translate(0.1f, 0, 0);
-        }
-
-        if (Input.GetKeyUp(KeyCode.Mouse0))
-        {
-
+            rotation *= Time.deltaTime;
+            translation *= Time.deltaTime;
+            transform.Rotate(0, rotation, 0);
+            transform.Translate(translation, 0, 0);
         }
 
         if (transform.position.y < 0 && cantidadDeTiros <= 10)

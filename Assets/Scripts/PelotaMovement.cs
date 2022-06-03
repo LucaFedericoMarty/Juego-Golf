7b29a58e0e8283cee7b8ae3e4d8860dd64414f50
LucaFedericoMarty <101400526+LucaFedericoMarty@users.noreply.github.com>
2022-06-02 21:28:24 -0300

    Comentarios y Cambios de Movimiento

--- Assets/Scripts/PelotaMovement.cs
+++ Assets/Scripts/PelotaMovement.cs
@@ -14,6 +14,7 @@ public class PelotaMovement : MonoBehaviour
     // Variable Float
     float timeElapsed;
     float horizonatlSpeed = 2.0f;
+    float verticalSpeed = -0.1f;
     public  float rotation;
     public float translation;
     Rigidbody rb;
@@ -23,23 +24,35 @@ public class PelotaMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         isPressing = false;
-        ganaste = true;
+        ganaste = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Si el click izquierdo es presionado, poner a isPressing como true
+
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {           
             isPressing = true;
         }
 
+        // Si se esta presionando el click izquierdo:
+        // Contar el tiempo por cuanto lo hizo
+        // Trackear cual fue el movimiento del mouse en el Eje X, luego multiplicar este valor por la velocidad de rotacion
+        // Trackear cual fue el movimiento del mouse en el Eje Y, luego multiplicar este valor por la velocidad de traslacion
+
         if (isPressing == true)
         {
             timeElapsed += Time.deltaTime;
             rotation = horizonatlSpeed * Input.GetAxis("Mouse X");
-            translation = Input.GetAxis("Mouse Y") * timeElapsed;
+            translation = verticalSpeed * Input.GetAxis("Mouse Y");
         }
+
+        // Si dejo de presionar el click izquierdo:
+        // La variable que monitorea el estado del click se torna falsa
+        // Se multiplica tanto el valor de rotacion como de traslacion por Time.DeltaTime, para que la velocidad no dependa de los FPS
+        // Se ejecuta el tiro
 
         if (Input.GetKeyUp(KeyCode.Mouse0))
         {

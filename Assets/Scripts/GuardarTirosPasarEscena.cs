59582bb516faea569e0863e61cc7ce4cd3d55cf2
LucaFedericoMarty <101400526+LucaFedericoMarty@users.noreply.github.com>
2022-06-05 23:24:21 -0300

    UI Avance

---
+++ Assets/Scripts/GuardarTirosPasarEscena.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GuardarTirosPasarEscena : MonoBehaviour
+{
+    public Text cantidadTiros;
+    public Text mensajeDeError;
+
+    public void guardarCantidadDeTiros()
+    {
+        int numeroDeTiros = int.Parse(cantidadTiros.text);
+
+        if (cantidadTiros.text == "")
+        {
+            mensajeDeError.text = "El campo no puede estar vacio";
+        }
+
+        else if (numeroDeTiros >= 30)
+        {
+            mensajeDeError.text = "Los tiros no pueden ser mas de 30";
+        }
+
+
+
+    }
+}
